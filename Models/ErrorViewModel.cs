1916d9705291f006c9ea3f710b066cb43e6b5990
Daniel <dekai@DANIEL>
2025-03-05 17:39:57 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clinica_Dental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
