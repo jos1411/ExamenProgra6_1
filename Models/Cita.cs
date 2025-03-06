805c0491364622e2caf77df579ec121054b43a1c
Daniel <dekai@DANIEL>
2025-03-05 21:44:53 -0600

    //Modelo de Pacientes y Cita

---
+++ Models/Cita.cs
@@ -0,0 +1,15 @@
+﻿namespace Clinica_Dental.Models
+{
+    public class Cita
+    {
+        public int Id_cita { get; set; }
+        public int Id_paciente { get; set; }
+        public System.DateTime Fecha_hora { get; set; }
+        public string Motivo_consulta { get; set; }
+        public string Estado { get; set; }
+        public System.DateTime Fecha_adicion { get; set; }
+        public string Adicionado_por { get; set; }
+        public System.DateTime? Fecha_modificacion { get; set; }
+        public string Modificado_por { get; set; }
+    }
+}
---
+++ Models/Pacientes.cs
@@ -0,0 +1,18 @@
+﻿namespace Clinica_Dental.Models
+{
+    public class Pacientes
+    {
+        public int Id_paciente { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Correo_electronico { get; set; }
+        public string Direccion { get; set; }
+        public System.DateTime Fecha_nacimiento { get; set; }
+        public System.DateTime Fecha_adicion { get; set; }
+        public string Adicionado_por { get; set; }
+        public System.DateTime? Fecha_modificacion { get; set; }
+        public string Modificado_por { get; set; }
+    }
+}
